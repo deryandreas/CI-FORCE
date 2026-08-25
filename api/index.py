a93c467a3ecb57c18689c14b379898476a8a1812
dery andreas tampubolon <dery@derys-MacBook-Air-6.local>
2026-08-25 17:21:13 +0700

    fix: append root directory to sys.path in api/index.py

--- api/index.py
+++ api/index.py
@@ -1,5 +1,9 @@
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Tambahkan direktori root ke sys.path agar modul project terbaca oleh Vercel
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'b2b_dashboard.settings')
 
