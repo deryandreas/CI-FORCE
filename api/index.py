09b2ddf8403e00e4a601c8d56ca444fd81dabc2f
dery andreas tampubolon <dery@derys-MacBook-Air-6.local>
2026-08-25 17:30:47 +0700

    fix: override DJANGO_SETTINGS_MODULE explicitly

--- api/index.py
+++ api/index.py
@@ -2,10 +2,10 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-# Tambahkan root direktori ke sys.path
+# Tambahkan direktori root ke path
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-# Gunakan 'core.settings', BUKAN 'b2b_dashboard.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+# Override langsung agar tidak memakai cache environment lama
+os.environ['DJANGO_SETTINGS_MODULE'] = 'core.settings'
 
 app = get_wsgi_application()
