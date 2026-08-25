6f616755eb0cc613cd08eacb3cf625752e9733d7
dery andreas tampubolon <dery@derys-MacBook-Air-6.local>
2026-08-25 17:28:07 +0700

    fix: change settings module from b2b_dashboard to core

--- api/index.py
+++ api/index.py
@@ -2,9 +2,10 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-# Tambahkan direktori root ke sys.path agar modul project terbaca oleh Vercel
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# Tambahkan root direktori ke sys.path
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'b2b_dashboard.settings')
+# Gunakan 'core.settings', BUKAN 'b2b_dashboard.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 app = get_wsgi_application()
